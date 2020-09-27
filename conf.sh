04f8ef6fa49cb18d2d578c7c5cbd241029bef2ed
Filip Peterek <fpeterek@seznam.cz>
2020-09-27 18:48:32 +0200

    Tweaked starting position

--- conf.sh
+++ conf.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-export START_LAT="49.8357131"
+export START_LAT="49.8360131"
 export START_LON="18.1621679"
-export SOCKET_DEBUG=1
+#export SOCKET_DEBUG=1
+export SOCKET_DEBUG=0
 export SERVER_HOST=localhost
 export SERVER_PORT=8080
+
